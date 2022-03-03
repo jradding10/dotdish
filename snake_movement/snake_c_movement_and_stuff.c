8fd0d99b5986cf1d03a44c5a1002c656ec257ba6
Jacqueline Radding <59897673+jradding10@users.noreply.github.com>
2022-03-03 14:44:24 -0800

    Update snake_c_movement_and_stuff.c

--- snake_movement/snake_c_movement_and_stuff.c
+++ snake_movement/snake_c_movement_and_stuff.c
@@ -27,7 +27,7 @@ void Direction_change { // if interrupt detected
         x_mv = 1; // direction is down
         y_mv = 0;
     }
-    else if BTNR_RD == 1{
+    else if BTNR_LD == 1{
         x_mv = -1; // direction is down
         y_mv = 0;
     }
