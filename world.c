ea7a5326eb9b79c9aa6c9eb3553d07d823267770
SeppWill <72676126+SeppWill@users.noreply.github.com>
2022-03-06 16:40:54 -0800

    World Version 2

--- world.c
+++ world.c
@@ -1,61 +1,21 @@
-bool world[80][60];
+#include <stdio.h>
 
-int apples[10][2] { {15, 11},
-                    {60, 30},
-                    {25, 42},
-                    {45, 47},
-                    {3,  20},
-                    {74, 49},
-                    {23, 78},
-                    {28, 77},
-                    {43, 27},
-                    {69, 13}};
-
-int a = 0;
-
-int snake[5][2] {{0,0}, {NULL, NULL}, {NULL, NULL}, {NULL, NULL}, {NULL, NULL}};
-
-int len = 1;
-int p = 0;
-int hx = 0;
-int hy = 0;
-
-world[snake[p][0]][snake[p][1]] = 1;
-world[apples[a][0]][apples[a][1]] = 1;
-
-void snakeMove (x, y) {
-    world[snake[p][0]][snake[p][1]] = 0;
-    snake[p][0] = hx + x; //Replaces oldest value with new location
-    snake[p][1] = hy + y;
-    world[snake[p][0]][snake[p][1]] = 1;
+void snakeMove (x, y, int snake[]) {
+    world[snake[0]][snake[1]] = 0;
+    snake[0] = hx + x; //Replaces oldest value with new location
+    snake[1] = hy + y;
+    world[snake[0]][snake[1]] = 1;
     hx += x; //Moves the head pointer
     hy += y;
-    if (p == len) //Rotates array pointer
-        p = 0;
-    else
-        p += 1;
 }
 
 void eat (x,y) {
-    int oldX = snake[p][0]; //Saves current location data
-    int oldY = snake[p][1];
-    snake[p][0] = hx+x; //Overwrites data
-    snake[p][1] = hy+y;
-    world[snake[p][0]][snake[p][1]] = 1;
-    for (i=p;i<=len+1;i++) { //Iterates through all items in array after current location pushing back one location
-        nextX = snake[i+1][0];
-        nextY = snake[i+1][1];
-        snake[i+1][0] = oldX;
-        snake[i+1][1] = oldY;
-        oldX = nextX;
-        oldY = nextY;
-    }
+    world[snake[0]][snake[1]] = 0;
+    snake[0] = hx+x; //Overwrites data
+    snake[1] = hy+y;
+    world[snake[0]][snake[1]] = 1;
     hx += x; //Moves the head pointer
     hy += y;
-    if (p == len) //Rotates array pointer 
-        p = 0;
-    else
-        p += 1;
     len += 1; //Adds 1 to length
     world[apples[a][0]][apples[a][1]] = 0;
     a += 1;
@@ -70,38 +30,65 @@ int BTNL_RD = 0;
 
 
 void main () {
+
+    int world[80][60];
+
+    int apples[10][2] = { {15, 11},
+                    {60, 30},
+                    {25, 42},
+                    {45, 47},
+                    {3,  20},
+                    {74, 49},
+                    {23, 78},
+                    {28, 77},
+                    {43, 27},
+                    {69, 13}};
+
+    int a = 0;
+
+    int snake[2] = {0,0};
+
+    int len = 1;
+    int hx = 0;
+    int hy = 0;
+
+    world[snake[0]][snake[1]] = 1;
+    world[apples[a][0]][apples[a][1]] = 1;
+
+    int done = 0;
     
-    if BTND_RD == 1 or BTNU_RD == 1 or BTNL_RD == 1 or BTNR_RD == 1
-    {
-        Directionchange()
+    while (done == 0) {
+        if (BTND_RD == 1 | BTNU_RD == 1 | BTNL_RD == 1 | BTNR_RD == 1)
+        {
+            int y_mv = 0;
+            int x_mv = 0;
+            if (BTNU_RD == 1){
+                y_mv = 1; //so we know direction is up
+                x_mv = 0;
+            }
+            else if (BTND_RD == 1){
+                y_mv = -1; // direction is down
+                x_mv = 0;
+            }
+            else if (BTNL_RD == 1){
+                x_mv = -1; // direction is left
+                y_mv = 0;
+            }
+            else if (BTNR_RD == 1){
+                x_mv = 1; // direction is right
+                y_mv = 0;
     }
-    else{
-    Delay()
+    snakeMove(x_mv, y_mv, snake)
+        }
+        else{
+            Delay()
+        }
     }
-    s_xcor += x_mv; // move based on the set direction
-    s_ycor += y_mv;// move y 
+
 }
 
 void Direction_change () { // if interrupt detected
-    int y_mv = 0;
-    int x_mv = 0;
-    if BTNU_RD == 1{
-        y_mv = 1; //so we know direction is up
-        x_mv = 0;
-    }
-    else if BTND_RD == 1{
-        y_mv = -1; // direction is down
-        x_mv = 0;
-    }
-    else if BTNL_RD == 1{
-        x_mv = -1; // direction is left
-        y_mv = 0;
-    }
-    else if BTNR_RD == 1{
-        x_mv = 1; // direction is right
-        y_mv = 0;
-    }
-    snakeMove(x_mv, y_mv)
+    
 }
 
 void delay () { // 250 milisecond dekat
