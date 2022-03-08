1b1cb6c5f06523e890b67f9b21251600bb9b56b8
SeppWill <72676126+SeppWill@users.noreply.github.com>
2022-03-08 13:05:50 -0800

    Update sepp_attempt.s

--- sepp_attempt.s
+++ sepp_attempt.s
@@ -1,4 +1,5 @@
 .eqv BG_COLOR, 0x0F	 # light blue (0/7 red, 3/7 green, 3/3 blue)
+.eqv dot_color, 0xFF
 .eqv VG_ADDR, 0x11000120
 .eqv VG_COLOR, 0x11000140
 .eqv BTNU_RD, 0x11001100
@@ -6,100 +7,183 @@
 .eqv BTNR_RD, 0x11001120
 .eqv BTNL_RD, 0x11001130
 
+.data
+	FOOD: .word 15, 11,   60, 30,   25, 42,   45, 47,   3, 20,   74, 49,   23, 38,   28, 57,   43, 27,   69, 13
+.text
 main:
     li sp, 0x10000     #initialize stack pointer
-	  li s2, VG_ADDR     #load MMIO addresses 
-	  li s3, VG_COLOR  
-    li s4, 25 # this is the snake's x cor
-    li s5, 35 # this is the snake's y cor
-    li s6, 0 # apple x coord
-    li s7, 0 # apply y coord
+    li s2, VG_ADDR     #load MMIO addresses 
+    li s3, VG_COLOR  
 
+    li s4, BTNU_RD  # button values
+    li s5, BTND_RD  
+    li s6, BTNR_RD 
+    li s7, BTNL_RD 
+    li a5, 25 # this is the snake's x cor
+    li a6, 35 # this is the snake's y cor
+    li a4, dot_color #dot color
 	# fill screen using default color
-	call draw_background  # must not modify s2, s3
-	
-    j loop
-	
-loop:
-	# check each button and move snake accordingly
-    li s1, BTNU_RD 
-    lw t1, 0(s1) #up button read
-    li s1, BTND_RD
-    lw t2, 0(s1) #down button read
-    li s1, BTNR_RD
-    lw t3, 0(s1) #right
-    li s1, BTNL_RD
-    lw t4, 0(s1) #left
-    li t6, 1
-    bne t1, t6, b0 #skip over if BTNU is pressed
-         addi s5, s5, 1 #add one to the y cor
-b0:  bne t2, t6, b1 # go to next if button is not pressed
-	 addi s5, s5, -1
-b1: bne t3, t6, b2 # go to next if button is not pressed
-	 addi s4, s4, 1
-b2:  bne t4, t6, b3 # go to next if button is not pressed 
-    	 addi s4, s4, -1
+    la s1, FOOD
+    li s0, 10
+    call draw_background  # must not modify s2, s3
+
+
+
+loop: # get new x cors 
     
-    	# check proximity to apple/food
-b3:    addi t5, s6, 1 # load x coord right of food
-    bne t1, s4, b4 # check if snake matches x coord
-    	bne s5, s7, b4 # check if snake matches y coord
+    lw s9, 0(s4) #up button read
+    lw s10, 0(s5) #down button read
+    lw s11, 0(s4) #right
+    lw x31, 0(s5) #left
+
+    beqz s9, b1#skip over if BTNU is pressed
+    call del_snake
+     addi a6, a6, 1
+    
+   b1: beqz s10, b2 #skip over if BTNU is pressed
+   call del_snake
+    addi a6, a6, -1
+    
+    b2:beqz s11, b3# go to next if button is not pressed
+    call del_snake
+     addi a5, a5, 1
+    
+    b3:beqz x31, b4 # go to next if button is not pressed 
+    call del_snake
+    addi a5, a5, -1
+     
+     b4: call draw_snake
+     
+    #eating code
+    lw t3, 0(s1) # load x food coord
+    lw t4, 4(s1) # load y food coord
+    addi t5, t4, 1 # load y coord above food
+    bne t5, a5, e0 # check if snake matches y coord
+    	bne t3, a6, e0 # check if snake matches x coord
     		call eat # calls eat function if x and y matches
-b4:    addi t5, s6, -1
-    bne t1, s4, b5
-    	bne s5, s7, b5
+    		addi s0, s0, -1
+    		j e3
+e0: addi t5, t4, -1
+    bne t5, a5, e1
+    	bne t3, a6, e1
     		call eat
-b5:    addi t5, s7, 1
-    bne t1, s5, b6
-    	bne s4, s6, b6
+    		addi s0, s0, -1
+    		j e3
+e1: addi t5, t3, 1
+    bne t5, a6, e2
+    	bne t4, a5, e2
     		call eat
-b6:    addi t5, s7, -1
-    bne t1, s5, b7
-    	bne s4, s6, b7
+    		addi s0, s0, -1
+    		j e3
+e2: addi t5, t3, -1
+    bne t5, a6, e3
+    	bne t4, a5, e3
     		call eat
+    		addi s0, s0, -1
+    		j e3
+     
+
+e3:    bnez s0, loop # loop until all food has been eaten
     
-b7:    call draw_dot
-    
-    beqz s11, loop
-
-
-
-#ISR: #changes button direction
-#    li s1, BTNU_RD 
-#    lw t1, 0(s1) #up button read
-#    li s1, BTND_RD
-#    lw t2, 0(s1) #down button read
-#    li s1, BTNR_RD
-#    lw t3, 0(s1) #right
-#    li s1, BTNL_RD
-#    lw t4, 0(s1) #left
-#    li t6, 1
-#    bne t1, t6, b8 #skip over if BTNU is pressed
-#         addi s5, s5, 1 #add one to the y cor
-#b8:    bne t2, t6, b9 # go to next if button is not pressed
-#	 addi s5, s5, -1
-#b9:    bne t3, t6, b10 # go to next if button is not pressed
-#	 addi s4, s4, 1
-#b10:    bne t4, t6, b11 # go to next if button is not pressed 
-#    	 addi s4, s4, -1
-#b11:    ret
-    
-
-eat: ret
+eat:
+        addi sp,sp,-4
+        sw ra, 0(sp)
+	andi t0,t3,0x7F	# select bottom 7 bits (col)
+	andi t1,t4,0x3F	# select bottom 6 bits  (row)
+	slli t1,t1,7	#  {a1[5:0],a0[6:0]} 
+	or t0,t1,t0	# 13-bit address
+	sw t0, 0(s2)	# write 13 address bits to register
+	sw a4, 0(s3)	# write color data to frame buffer
+	
+	addi s1, s1, 8	# iterate to next food address
+	lw t3, 0(s1)	# load x food coord
+    	lw t4, 4(s1)	# load y food coord
+	
+	andi t0,t3,0x7F	# select bottom 7 bits (col)
+	andi t1,t4,0x3F	# select bottom 6 bits  (row)
+	slli t1,t1,7	#  {a1[5:0],a0[6:0]} 
+	or t0,t1,t0	# 13-bit address
+	sw t0, 0(s2)	# write 13 address bits to register
+	sw a3, 0(s3)	# write color data to frame buffer
+	addi sp,sp,4
+	ret
 
 draw_dot:
+       addi sp,sp,-4
+       sw ra, 0(sp)
 	andi t0,a0,0x7F	# select bottom 7 bits (col)
 	andi t1,a1,0x3F	# select bottom 6 bits  (row)
 	slli t1,t1,7	#  {a1[5:0],a0[6:0]} 
 	or t0,t1,t0	# 13-bit address
 	sw t0, 0(s2)	# write 13 address bits to register
 	sw a3, 0(s3)	# write color data to frame buffer
+	addi sp,sp,4
 	ret
 
+draw_snake:
+        addi sp,sp,-4
+        sw ra, 0(sp)
+	andi t0,a5,0x7F	# select bottom 7 bits (col)
+	andi t1,a6,0x3F	# select bottom 6 bits  (row)
+	slli t1,t1,7	#  {a1[5:0],a0[6:0]} 
+	or t0,t1,t0	# 13-bit address
+	sw t0, 0(s2)	# write 13 address bits to register
+	sw a4, 0(s3)	# write color data to frame buffer
+	addi sp,sp,4
+	ret
+del_snake:
+        addi sp,sp,-4
+        sw ra, 0(sp)
+	andi t0,a5,0x7F	# select bottom 7 bits (col)
+	andi t1,a6,0x3F	# select bottom 6 bits  (row)
+	slli t1,t1,7	#  {a1[5:0],a0[6:0]} 
+	or t0,t1,t0	# 13-bit address
+	sw t0, 0(s2)	# write 13 address bits to register
+	sw a3, 0(s3)	# write color data to frame buffer
+	addi sp,sp,4
+	ret
+
+# draws a horizontal line from (a0,a1) to (a2,a1) using color in a3
+# Modifies (directly or indirectly): t0, t1, a0, a2
+draw_horizontal_line:
+	addi sp,sp,-4
+	sw ra, 0(sp)
+	addi a2,a2,1	#go from a0 to a2 inclusive
+draw_horiz1:
+	call draw_dot  # must not modify: a0, a1, a2, a3
+	addi a0,a0,1
+	bne a0,a2, draw_horiz1
+	lw ra, 0(sp)
+	addi sp,sp,4
+	ret
+
+# draws a vertical line from (a0,a1) to (a0,a2) using color in a3
+# Modifies (directly or indirectly): t0, t1, a1, a2
+draw_vertical_line:
+	addi sp,sp,-4
+	sw ra, 0(sp)
+	addi a2,a2,1
+draw_vert1:
+	call draw_dot  # must not modify: a0, a1, a2, a3
+	addi a1,a1,1
+	bne a1,a2,draw_vert1
+	lw ra, 0(sp)
+	addi sp,sp,4
+	ret
+
+# Fills the 60x80 grid with one color using successive calls to draw_horizontal_line
+# Modifies (directly or indirectly): t0, t1, t4, a0, a1, a2, a3
 draw_background:
 	addi sp,sp,-4
 	sw ra, 0(sp)
 	li a3, BG_COLOR	#use default color
 	li a1, 0	#a1= row_counter
 	li t4, 60 	#max rows
+start:	li a0, 0
+	li a2, 79 	#total number of columns
+	call draw_horizontal_line  # must not modify: t4, a1, a3
+	addi a1,a1, 1
+	bne t4,a1, start	#branch to draw more rows
+	lw ra, 0(sp)
+	addi sp,sp,4
 	ret
