59b59c1a61b3f4a6321f86a89b0d67c09a9de9b4
SeppWill <72676126+SeppWill@users.noreply.github.com>
2022-03-08 16:20:49 -0800

    Update sepp_attempt.s

--- sepp_attempt.s
+++ sepp_attempt.s
@@ -1,15 +1,19 @@
 .eqv BG_COLOR, 0x0F	 # light blue (0/7 red, 3/7 green, 3/3 blue)
+.eqv delay_num, 0x1000000
 .eqv dot_color, 0xFF
+.eqv  food_color, 0x04
 .eqv VG_ADDR, 0x11000120
 .eqv VG_COLOR, 0x11000140
 .eqv BTNU_RD, 0x11001100
 .eqv BTND_RD, 0x11001110
-.eqv BTNR_RD, 0x11001120
-.eqv BTNL_RD, 0x11001130
-
+.eqv BTNL_RD, 0x11001120
+.eqv BTNR_RD, 0x11001130
 .data
-	FOOD: .word 15, 11,   60, 30,   25, 42,   45, 47,   3, 20,   74, 49,   23, 38,   28, 57,   43, 27,   69, 13
+FOOD: .word 15, 11,   60, 30,   25, 42,   45, 47,   3, 20,   74, 49,   23, 38,   28, 57,   43, 27,   69, 13
 .text
+
+.global main 
+.type main, @function
 main:
     li sp, 0x10000     #initialize stack pointer
     li s2, VG_ADDR     #load MMIO addresses 
@@ -23,18 +27,17 @@ main:
     li a6, 35 # this is the snake's y cor
     li a4, dot_color #dot color
 	# fill screen using default color
-    la s1, FOOD
-    li s0, 10
     call draw_background  # must not modify s2, s3
 
 
 
 loop: # get new x cors 
+    # set food and food cors
     
     lw s9, 0(s4) #up button read
     lw s10, 0(s5) #down button read
-    lw s11, 0(s4) #right
-    lw x31, 0(s5) #left
+    lw s11, 0(s6) #right
+    lw x31, 0(s7) #left
 
     beqz s9, b1#skip over if BTNU is pressed
     call del_snake
@@ -53,6 +56,8 @@ loop: # get new x cors
     addi a5, a5, -1
      
      b4: call draw_snake
+     #call draw_food
+call draw_food
      
     #eating code
     lw t3, 0(s1) # load x food coord
@@ -60,47 +65,66 @@ loop: # get new x cors
     addi t5, t4, 1 # load y coord above food
     bne t5, a5, e0 # check if snake matches y coord
     	bne t3, a6, e0 # check if snake matches x coord
-    		call eat # calls eat function if x and y matches
+    		call del_food
+    		addi s1, s1, 8	# iterate to next food address
+		lw t3, 0(s1)	# load x food coord
+    		lw t4, 4(s1)	# load y food coord
+    		call draw_food
     		addi s0, s0, -1
     		j e3
 e0: addi t5, t4, -1
     bne t5, a5, e1
     	bne t3, a6, e1
-    		call eat
+    		call del_food
+    		addi s1, s1, 8	# iterate to next food address
+		lw t3, 0(s1)	# load x food coord
+    		lw t4, 4(s1)	# load y food coord
+    		call draw_food
     		addi s0, s0, -1
     		j e3
 e1: addi t5, t3, 1
     bne t5, a6, e2
     	bne t4, a5, e2
-    		call eat
+    		call del_food
+    		addi s1, s1, 8	# iterate to next food address
+		lw t3, 0(s1)	# load x food coord
+    		lw t4, 4(s1)	# load y food coord
+    		call draw_food
     		addi s0, s0, -1
     		j e3
 e2: addi t5, t3, -1
     bne t5, a6, e3
     	bne t4, a5, e3
-    		call eat
+    		call del_food
+    		addi s1, s1, 8	# iterate to next food address
+		lw t3, 0(s1)	# load x food coord
+    		lw t4, 4(s1)	# load y food coord
+    		call draw_food
     		addi s0, s0, -1
     		j e3
      
 
-e3:    bnez s0, loop # loop until all food has been eaten
-    
-eat:
+e3:    bnez s0, loop
+     call delay
+    j loop
+
+draw_food:
         addi sp,sp,-4
         sw ra, 0(sp)
-	andi t0,t3,0x7F	# select bottom 7 bits (col)
-	andi t1,t4,0x3F	# select bottom 6 bits  (row)
+	andi t0,t4,0x7F	# select bottom 7 bits (col)
+	andi t1,t3,0x3F	# select bottom 6 bits  (row)
 	slli t1,t1,7	#  {a1[5:0],a0[6:0]} 
 	or t0,t1,t0	# 13-bit address
 	sw t0, 0(s2)	# write 13 address bits to register
 	sw a4, 0(s3)	# write color data to frame buffer
+	addi sp,sp,4
+	ret
 	
-	addi s1, s1, 8	# iterate to next food address
-	lw t3, 0(s1)	# load x food coord
-    	lw t4, 4(s1)	# load y food coord
-	
-	andi t0,t3,0x7F	# select bottom 7 bits (col)
-	andi t1,t4,0x3F	# select bottom 6 bits  (row)
+del_food:
+        addi sp,sp,-4
+        sw ra, 0(sp)
+	andi t0,t4,0x7F	# select bottom 7 bits (col)
+	andi t1,t3,0x3F	# select bottom 6 bits  (row)
 	slli t1,t1,7	#  {a1[5:0],a0[6:0]} 
 	or t0,t1,t0	# 13-bit address
 	sw t0, 0(s2)	# write 13 address bits to register
@@ -123,17 +147,7 @@ draw_dot:
 draw_snake:
         addi sp,sp,-4
         sw ra, 0(sp)
-	andi t0,a5,0x7F	# select bottom 7 bits (col)
-	andi t1,a6,0x3F	# select bottom 6 bits  (row)
-	slli t1,t1,7	#  {a1[5:0],a0[6:0]} 
-	or t0,t1,t0	# 13-bit address
-	sw t0, 0(s2)	# write 13 address bits to register
-	sw a4, 0(s3)	# write color data to frame buffer
-	addi sp,sp,4
-	ret
-del_snake:
-        addi sp,sp,-4
-        sw ra, 0(sp)
+         li a3, dot_color #dot color
 	andi t0,a5,0x7F	# select bottom 7 bits (col)
 	andi t1,a6,0x3F	# select bottom 6 bits  (row)
 	slli t1,t1,7	#  {a1[5:0],a0[6:0]} 
@@ -142,6 +156,29 @@ del_snake:
 	sw a3, 0(s3)	# write color data to frame buffer
 	addi sp,sp,4
 	ret
+	
+del_snake: # deletes snake location after it moves
+        addi sp,sp,-4
+        sw ra, 0(sp)
+	andi t0,a5,0x7F	# select bottom 7 bits (col)
+	andi t1,a6,0x3F	# select bottom 6 bits  (row)
+	slli t1,t1,7	#  {a1[5:0],a0[6:0]} 
+	li a3, BG_COLOR #dot color
+	or t0,t1,t0	# 13-bit address
+	sw t0, 0(s2)	# write 13 address bits to register
+	sw a3, 0(s3)	# write color data to frame buffer
+	addi sp,sp,4
+	ret
+	
+delay:addi sp,sp,-4
+     sw ra, 0(sp)
+     li a2, 0
+     li a1, 1700000
+delay_int:
+    addi a2,a2, 1
+    bne a2, a1, delay_int
+    addi sp,sp,4
+    ret
 
 # draws a horizontal line from (a0,a1) to (a2,a1) using color in a3
 # Modifies (directly or indirectly): t0, t1, a0, a2
